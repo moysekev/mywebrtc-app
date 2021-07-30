dcc9c5c56682a7cc9e826fae8259037c0559c886
Kevin Moyse <kevin.moyse@apizee.com>
2021-07-30 17:07:04 +0200

    continued fb login

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -142,52 +142,6 @@ export class HomeComponent implements AfterViewInit, OnDestroy {
 
   }
 
-  doFbSignIn(): Promise<firebase.User> {
-    return new Promise<firebase.User>((resolve, reject) => {
-      const provider = new firebase.auth.FacebookAuthProvider();
-      firebase.auth().languageCode = 'fr';
-      provider.setCustomParameters({
-        'display': 'popup'
-      });
-      firebase
-        .auth()
-        .signInWithPopup(provider)
-        .then((result) => {
-          /** @type {firebase.auth.OAuthCredential} */
-          const credential: firebase.auth.OAuthCredential | null = result.credential;
-          // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-          //var accessToken = credential.accessToken;
-
-          if (credential === null) {
-            console.error('signInWithPopup', result);
-            return;
-          }
-          console.log('signInWithPopup', result);
-
-          // The signed-in user info.
-          const user = result.user;
-          if (user === null) {
-            reject("signInWithPopup: user is null");
-            return;
-          }
-
-          resolve(user);
-        })
-        .catch((error) => {
-          console.error("signInWithPopup", error);
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          // The email of the user's account used.
-          var email = error.email;
-          // The firebase.auth.AuthCredential type that was used.
-          var credential = error.credential;
-
-          reject(error);
-        });
-    });
-  }
-
   ngAfterViewInit() {
     navigator.mediaDevices.getUserMedia({
       video: true,
@@ -202,6 +156,11 @@ export class HomeComponent implements AfterViewInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.doCleanUp();
+
+  }
+
+  public signOut() {
+    firebase.auth().signOut().then(() => { console.log('signed Out') });
   }
 
   // --------------------------------------------------------------------------
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import firebase from 'firebase';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  doFbSignIn(): Promise<firebase.User> {
+    return new Promise<firebase.User>((resolve, reject) => {
+      const provider = new firebase.auth.FacebookAuthProvider();
+      firebase.auth().languageCode = 'fr';
+      provider.setCustomParameters({
+        'display': 'popup'
+      });
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+          /** @type {firebase.auth.OAuthCredential} */
+          const credential: firebase.auth.OAuthCredential | null = result.credential;
+          // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+          //var accessToken = credential.accessToken;
+
+          if (credential === null) {
+            console.error('signInWithPopup', result);
+            return;
+          }
+          console.log('signInWithPopup', result);
+
+          // The signed-in user info.
+          const user = result.user;
+          if (user === null) {
+            reject("signInWithPopup: user is null");
+            return;
+          }
+
+          resolve(user);
+
+          this.router.navigate(['/home']);
+        })
+        .catch((error) => {
+          console.error("signInWithPopup", error);
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+
+          reject(error);
+        });
+    });
+  }
 }
