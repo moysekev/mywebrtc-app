79a9c29fa3f7147b50dd7cdfcfe8ed41c0417302
kmoyse <kevin.moyse@apizee.com>
2024-01-10 16:09:40 +0100

    consts->constants

---
+++ src/app/constants.ts
@@ -0,0 +1,7 @@
+export const USERS = 'App_Users';
+
+export const DATACHANNEL_SNAPSHOT_PATH = 'snapshot';
+export const DATACHANNEL_SNAPSHOT_CHUNK_SIZE = 16000;
+export const DATACHANNEL_SNAPSHOT_END = '\n';
+
+export const DATACHANNEL_POINTER_PATH = 'pointer';
